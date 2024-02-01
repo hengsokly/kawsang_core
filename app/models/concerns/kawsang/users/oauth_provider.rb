e377f947e761a8ae6765a6499f44afb0452bde40
Sokly <hengsokly23@gmail.com>
2024-02-01 16:44:44 +0700

    Oauth2 provider: add doorkeeper gem

---
+++ app/models/concerns/kawsang/users/oauth_provider.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Kawsang
+  module Users
+    module OauthProvider
+      extend ActiveSupport::Concern
+
+      included do
+        # Door keeper association
+        has_many :access_grants,
+                  class_name: "Doorkeeper::AccessGrant",
+                  foreign_key: :resource_owner_id,
+                  dependent: :delete_all # or :destroy if you need callbacks
+
+        has_many :access_tokens,
+                  class_name: "Doorkeeper::AccessToken",
+                  foreign_key: :resource_owner_id,
+                  dependent: :delete_all # or :destroy if you need callbacks
+      end
+    end
+  end
+end
