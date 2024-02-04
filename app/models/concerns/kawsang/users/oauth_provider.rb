c9cc768069d5d1a9d786816f4cff6c75f2f35585
Sokly <hengsokly23@gmail.com>
2024-02-05 02:56:12 +0700

    Doorkeeper table: allow redirect_url to nullable

--- app/models/concerns/kawsang/users/oauth_provider.rb
+++ app/models/concerns/kawsang/users/oauth_provider.rb
@@ -16,6 +16,13 @@ module Kawsang
                   class_name: "Doorkeeper::AccessToken",
                   foreign_key: :resource_owner_id,
                   dependent: :delete_all # or :destroy if you need callbacks
+
+        # https://rubyyagi.com/rails-api-authentication-devise-doorkeeper/
+        # the authenticate method from devise documentation
+        def self.authenticate(email, password)
+          user = User.find_for_authentication(email: email)
+          user&.valid_password?(password) ? user : nil
+        end
       end
     end
   end
